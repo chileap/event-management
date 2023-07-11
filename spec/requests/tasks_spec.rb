312f4e5f9369b72355aed5b06df986c1b0e7fd3c
Chileap Chhin <chileapchhin@gmail.com>
2023-07-11 12:05:18 +0700

    fixed standardrb

--- spec/requests/tasks_spec.rb
+++ spec/requests/tasks_spec.rb
@@ -1,4 +1,4 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe "Tasks", type: :request do
   describe "GET /index" do
@@ -11,5 +11,4 @@ RSpec.describe "Tasks", type: :request do
       expect(response).to have_http_status(:success)
     end
   end
-
 end
--- spec/views/tasks/index.html.erb_spec.rb
+++ spec/views/tasks/index.html.erb_spec.rb
@@ -1,8 +1,8 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe "tasks/index.html.erb", type: :view do
   it "displays all the tasks" do
     render
-    expect(rendered).to match /Task/
+    expect(rendered).to match(/Task/)
   end
 end
