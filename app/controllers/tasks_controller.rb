30a889bba69b19bc159015673551132bcdeb0e1f
Chileap Chhin <chileapchhin@gmail.com>
2023-07-11 12:04:33 +0700

    setup task into app

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,4 @@
+class TasksController < BaseController
+  def index
+  end
+end
---
+++ spec/requests/tasks_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe "Tasks", type: :request do
+  describe "GET /index" do
+    before do
+      sign_in create(:user)
+    end
+
+    it "returns http success" do
+      get "/tasks"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
